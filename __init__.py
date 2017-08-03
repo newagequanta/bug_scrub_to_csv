fcb8a37d28d46aa726f2d3f6c803c10dedf576ba
NewAgeQuanta <newagequanta@gmail.com>
2017-08-03 15:02:07 -0400

    recover __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ This will call create_csv in bug_scrub_to_csv module
 #code.py is the file with all the main functions
 from . import code
 __copyright__ = "Copyright (c) 2017 Cisco Systems. All rights reserved."
-
 def task(Env, input_filename, output_filename):
     """
     Accepts a bug scrub from GDC (in unicode-8) and produces a CSV with relevant information 
@@ -14,7 +13,6 @@ def task(Env, input_filename, output_filename):
     https://scripts.cisco.com/doc/
     Browse more examples in BDB starting with "bdblib_":
     https://scripts.cisco.com/ui/browse/used/0/bdblib_
-
     """
     
     #create_csv is the function inside code that accepts filenames and starts
