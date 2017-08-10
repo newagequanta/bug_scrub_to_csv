ec56c2d1f1d05646c9331425e7049bba6ebe090a
nimbhasi <nimbhasi@cisco.com>
2017-08-10 14:21:59 +0000

    /var/bdb/nas/tasks/bug_scrub_to_csv updated by nimbhasi

--- test_task.py
+++ test_task.py
@@ -3,8 +3,13 @@ pytest unit tests
 Functions should start with ``test_`` and contain a list of assertions that evaluate to True if the test succeeds.
 For more info see: http://pytest.org/latest/
 """
-
+import sys
 import task_bug_scrub_to_csv
+
+sys.path = ['', '/venv/python361-1/lib/python36.zip', '/venv/python361-1/lib/python3.6', 
+'/venv/python361-1/lib/python3.6/lib-dynload', '/home/bdb/.pyenv/versions/3.6.1/lib/python3.6',
+'/venv/python361-1/lib/python3.6/site-packages', '/venv/python361-1/src/jtextfsm']
+
 def test_task():
     try:
         task_bug_scrub_to_csv.task(None,
