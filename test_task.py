2b78a139b4e35a6c0bfcdfe2fc07c2db6461d1e7
nimbhasi <nimbhasi@cisco.com>
2017-08-10 14:34:52 +0000

    /var/bdb/nas/tasks/bug_scrub_to_csv updated by nimbhasi

--- test_task.py
+++ test_task.py
@@ -16,7 +16,7 @@ def test_task():
         task_bug_scrub_to_csv.task(None,
                                '/var/bdb/sessions/sandboxed-user/test.doc',
                                '/var/bdb/sessions/sandboxed-user/tests/bug_scrub_to_csv/output.csv') == True
-        assert hashlib.sha256(open('output.csv', 'rb').read()).hexdigest() == '622afdfa8a3dc452a6387364bc15872275552715a8834b020b5ca8e09455edfa'
+        assert hashlib.sha256(open('output.csv', 'rb').read()).hexdigest() == '3c2bc6beed31de87f4e6c6114f9ac54a30ec420cf6f160208627a2794f1e61bf'
     except AttributeError:
         assert True
     except:
