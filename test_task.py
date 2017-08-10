a8f08b68433d9f49282b195ae2b5ab82e273456f
NewAgeQuanta <newagequanta@gmail.com>
2017-08-09 23:07:47 -0400

    adding test file full file paths

--- test_task.py
+++ test_task.py
@@ -6,4 +6,7 @@ For more info see: http://pytest.org/latest/
 
 import task_bug_scrub_to_csv
 def test_task():
+    task_bug_scrub_to_csv.task(None,
+                               '/var/bdb/sessions/sandboxed-user/test.doc',
+                               '/var/bdb/sessions/sandboxed-user/result.csv')
     assert True == True
